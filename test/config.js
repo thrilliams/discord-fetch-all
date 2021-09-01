6555650dc4a7b7e2fb5e5b5fe1c8c3351e2a1a91
Soren <soren@aethre.co>
2021-08-31 22:01:37 -0700

    update tsconfig

--- test/config.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.BOT_TOKEN = void 0;
-exports.BOT_TOKEN = 'Nzc0MTM2MjI4Mjg2ODI0NDc5.X6TYiw.h8JzpWmiGJDnbZphxexJPY1-2TA';
